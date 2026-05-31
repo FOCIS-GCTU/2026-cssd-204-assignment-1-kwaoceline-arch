e59e9bff97208005e0d0f6317b2524dcac40283c
Kwao Joyceline Akyiah <kwaoceline@gmail.com>
2026-05-30 18:43:46 -0700

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,29 +1,33 @@
-#File: initials.py
-#Description: Prints large block initials (KJA) using strings and print functions
-#Assignment Number: 1
+# File: initials.py
+# Description: Prints stylized large block initials KJA using strings.
+# Assignment Number: 1
 #
-#Name: Kwao Joyceline Akyiah
-#STUDENT ID: 2425402088
-#Email: 2425402088@live.gctu.edu.gh
-#Grader: Augustus Buckman
+# Name: Kwao Joyceline Akyiah
+# STUDENT ID: 2425402088
+# Email: 2425402088@live.gctu.edu.gh
+# Grader: Augustus Buckman
 #
-#On my honor, Kwao Joyceline Akyiah, this programming assignment is my own work
-#and I have not provided this code to anyone
+# On my honor, Kwao Joyceline Akyiah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
+    """Prints large stylized initials KJA with periods."""
     print()
     print("...KJA")
     print()
-
-    print("...KKK......KKK......JJJJJJJJJJJJ......AAAAAAAA....")
-    print("...KKK....KKK...........JJJJJJ........AAAAAAAAAA...")
-    print("...KKK..KKK.............JJJJJJ.......AAA......AAA..")
-    print("...KKKKKK...............JJJJJJ......AAA........AAA.")
-    print("...KKKKKK...............JJJJJJ......AAAAAAAAAAAAAA.")
-    print("...KKK..KKK.............JJJJJJ......AAAAAAAAAAAAAA.")
-    print("...KKK....KKK.....JJJ...JJJJJJ......AAA........AAA.")
-    print("...KKK......KKK...JJJ...JJJJJJ......AAA........AAA.")
-    print("...KKK......KKK...JJJJJJJJJJJJ..**..AAA........AAA..**")
-    print("...KKK......KKK.....JJJJJJJJ....**..AAA........AAA..**")
+    print("...KKK......KKK.......JJJJJJJJJJJJ......AAAAAAAA....")
+    print("...KKK....KKK............JJJJJJ........AAAAAAAAAA...")
+    print("...KKK..KKK..............JJJJJJ.......AAA......AAA..")
+    print("...KKKKKK................JJJJJJ......AAA........AAA.")
+    print("...KKKKKK................JJJJJJ......AAAAAAAAAAAAAA.")
+    print("...KKK..KKK..............JJJJJJ......AAAAAAAAAAAAAA.")
+    print("...KKK....KKK......JJJ...JJJJJJ......AAA........AAA.")
+    print("...KKK......KKK....JJJ...JJJJJJ......AAA........AAA.")
+    print("...KKK.......KKK...JJJJJJJJJJJJ..**..AAA........AAA..**")
+    print("...KKK........KKK....JJJJJJJJ....**..AAA........AAA..**")
     print()
-    main()
+
+
+main()
+
