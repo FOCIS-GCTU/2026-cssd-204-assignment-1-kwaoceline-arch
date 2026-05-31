c2fda9b97ac5c2eab99371b355ec49c79cbad171
Kwao Joyceline Akyiah <kwaoceline@gmail.com>
2026-05-31 01:29:25 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,29 @@
+#File: initials.py
+#Description: Prints large block initials (KJA) using strings and print functions
+#Assignment Number: 1
+#
+#Name: Kwao Joyceline Akyiah
+#STUDENT ID: 2425402088
+#Email: 2425402088@live.gctu.edu.gh
+#Grader: Augustus Buckman
+#
+#On my honor, Kwao Joyceline Akyiah, this programming assignment is my own work
+#and I have not provided this code to anyone
 
 def main():
-  pass #code goes here
+    print()
+    print("...KJA")
+    print()
 
-
-
-main()
+    print("...KKK......KKK......JJJJJJJJJJJJ......AAAAAAAA....")
+    print("...KKK....KKK...........JJJJJJ........AAAAAAAAAA...")
+    print("...KKK..KKK.............JJJJJJ.......AAA......AAA..")
+    print("...KKKKKK...............JJJJJJ......AAA........AAA.")
+    print("...KKKKKK...............JJJJJJ......AAAAAAAAAAAAAA.")
+    print("...KKK..KKK.............JJJJJJ......AAAAAAAAAAAAAA.")
+    print("...KKK....KKK.....JJJ...JJJJJJ......AAA........AAA.")
+    print("...KKK......KKK...JJJ...JJJJJJ......AAA........AAA.")
+    print("...KKK......KKK...JJJJJJJJJJJJ..**..AAA........AAA..**")
+    print("...KKK......KKK.....JJJJJJJJ....**..AAA........AAA..**")
+    print()
+    main()
